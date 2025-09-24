ffb585b42b6b69d6840b266a8d7046f44729dab5
Amruta Velamuri <amvelamuri@gmail.com>
2025-09-24 18:55:35 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+
+import streamlit as st
+
+st.title("My Simple App")
+
+# Button 1
+if st.button("Button 1"):
+    st.write("You clicked Button 1!")
+
+# Button 2
+if st.button("Button 2"):
+    st.write("You clicked Button 2!")
